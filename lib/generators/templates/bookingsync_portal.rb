4788ced1be00b8bf55b5b1ca5573d697fef4ca42
Piotr Marciniak <mandaryyyn@gmail.com>
2015-02-25 10:09:34 +0100

    add callbacks to initializer template

--- lib/generators/templates/bookingsync_portal.rb
+++ lib/generators/templates/bookingsync_portal.rb
@@ -12,3 +12,18 @@
 # BookingsyncPortal.rental_resource = '::Admin::RentalResource'
 # BookingsyncPortal.remote_rental_resource = '::Admin::RemoteRentalResource'
 # BookingsyncPortal.connection_resource = '::Admin::ConnectionResource'
+
+module BookingsyncPortal::Callbacks
+  def self.connection_created(connection)
+    # handle synchronization of rentals after connection is made
+  end
+
+  def self.connection_destroyed(connection)
+    # handle synchronization of rentals after connection is destroyed
+  end
+
+  def self.fetch_remote_rentals(account)
+    # fetch remote rentals
+    # return false if remote account is not present or not valid
+  end
+end
