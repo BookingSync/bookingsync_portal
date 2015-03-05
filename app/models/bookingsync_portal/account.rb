2ad7c4ffa96fb0ce4905888f144deddc3a9c9915
Sébastien Grosjean <public@zencocoon.com>
2015-03-04 16:00:33 -0800

    Make sure to destroy relations that are dependent

--- app/models/bookingsync_portal/account.rb
+++ app/models/bookingsync_portal/account.rb
@@ -3,8 +3,9 @@ class BookingsyncPortal::Account < ActiveRecord::Base
 
   include BookingSync::Engine::Model
 
-  has_many :remote_accounts, class_name: BookingsyncPortal.remote_account_model
-  has_many :rentals, class_name: BookingsyncPortal.rental_model
-  has_many :remote_rentals, through: :remote_accounts, class_name: BookingsyncPortal.remote_rental_model
+  has_many :remote_accounts, class_name: BookingsyncPortal.remote_account_model, dependent: :destroy
+  has_many :rentals, class_name: BookingsyncPortal.rental_model, dependent: :destroy
+  has_many :remote_rentals, through: :remote_accounts,
+    class_name: BookingsyncPortal.remote_rental_model
   has_many :connections, through: :rentals, class_name: BookingsyncPortal.connection_model
 end
