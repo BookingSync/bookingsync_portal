7c4d227b7292f0793db268c16f41da5364460e0f
Fredar <a.krzeminski.freda@gmail.com>
2015-07-31 11:31:21 +0200

    Update test schema

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -14,33 +14,26 @@
 ActiveRecord::Schema.define(version: 20150521091056) do
 
   create_table "accounts", force: :cascade do |t|
-    t.string   "provider",            limit: 255
+    t.string   "provider"
     t.integer  "uid"
-    t.string   "name",                limit: 255
-    t.string   "oauth_access_token",  limit: 255
-    t.string   "oauth_refresh_token", limit: 255
+    t.string   "name"
+    t.string   "oauth_access_token"
+    t.string   "oauth_refresh_token"
     t.datetime "oauth_expires_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "availabilities", force: :cascade do |t|
-    t.integer  "rental_id"
-    t.integer  "synced_id"
+    t.text     "synced_data"
     t.datetime "synced_all_at"
-    t.string   "map",           limit: 255
-    t.date     "start_date"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "email"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
-  add_index "availabilities", ["rental_id"], name: "index_availabilities_on_rental_id"
+  add_index "accounts", ["uid"], name: "index_accounts_on_uid"
 
   create_table "connections", force: :cascade do |t|
     t.integer  "remote_rental_id"
     t.integer  "rental_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
   end
 
   add_index "connections", ["remote_rental_id"], name: "index_connections_on_remote_rental_id"
@@ -52,28 +45,30 @@ ActiveRecord::Schema.define(version: 20150521091056) do
     t.text     "synced_data"
     t.datetime "synced_all_at"
     t.integer  "position"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   add_index "photos", ["rental_id"], name: "index_photos_on_rental_id"
+  add_index "photos", ["synced_id"], name: "index_photos_on_synced_id"
 
   create_table "rates", force: :cascade do |t|
     t.integer  "rental_id"
     t.integer  "synced_id"
     t.text     "synced_data"
     t.datetime "synced_all_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   add_index "rates", ["rental_id"], name: "index_rates_on_rental_id"
+  add_index "rates", ["synced_id"], name: "index_rates_on_synced_id"
 
   create_table "remote_accounts", force: :cascade do |t|
     t.integer  "account_id"
     t.integer  "uid"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   add_index "remote_accounts", ["account_id"], name: "index_remote_accounts_on_account_id"
@@ -83,8 +78,8 @@ ActiveRecord::Schema.define(version: 20150521091056) do
     t.integer  "uid"
     t.text     "remote_data"
     t.datetime "synchronized_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   add_index "remote_rentals", ["remote_account_id"], name: "index_remote_rentals_on_remote_account_id"
@@ -96,10 +91,11 @@ ActiveRecord::Schema.define(version: 20150521091056) do
     t.datetime "synced_all_at"
     t.integer  "position"
     t.datetime "published_at"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   add_index "rentals", ["account_id"], name: "index_rentals_on_account_id"
+  add_index "rentals", ["synced_id"], name: "index_rentals_on_synced_id"
 
 end
