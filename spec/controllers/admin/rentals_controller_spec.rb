6bd6483522c52067581991f897bab93a8f0e3018
Adrian Fernandez <adrianfernandez85@gmail.com>
2018-05-09 19:20:12 +0200

    Fix specs

--- spec/controllers/admin/rentals_controller_spec.rb
+++ spec/controllers/admin/rentals_controller_spec.rb
@@ -31,16 +31,24 @@ describe BookingsyncPortal::Admin::RentalsController do
     end
     
     context "using custom methods" do
-      let!(:fake_connected_rental) { create(:rental, account: account) }
-      let!(:fake_visible_rental) { create(:rental, account: account) }
-      let!(:fake_remote_account) { create(:remote_account, account: account) }
-      let!(:fake_remote_rental) { create(:remote_rental, account: account) }
+      let(:fake_connected_rental) { create(:rental, account: account) }
+      let(:fake_visible_rental) { create(:rental, account: account) }
+      let(:fake_remote_account) { create(:remote_account, account: account) }
+      let(:fake_remote_rental) { create(:remote_rental, account: account) }
+
+      let(:fake_connected_rentals_value) { [fake_connected_rental] }
+      let(:fake_visible_rental_value) { [fake_visible_rental] }
+      let(:fake_remote_accounts_value) { [fake_remote_account] }
+      let(:fake_remote_rentals_by_account_value) do
+        RemoteRental.where(id: fake_remote_rental).includes(:remote_account, :rental).group_by(&:remote_account)
+      end
+
       before do
         BookingsyncPortal.setup do |config|
-          config.not_connected_rentals = lambda { |account| [fake_connected_rental] }
-          config.visible_rentals = lambda { |account| [fake_visible_rental] }
-          config.remote_accounts = lambda { |account| [fake_remote_account] }
-          config.remote_rentals_by_account = lambda { |account| [fake_remote_rental] }
+          config.not_connected_rentals = lambda { |account| fake_connected_rentals_value }
+          config.visible_rentals = lambda { |account| fake_visible_rental_value }
+          config.remote_accounts = lambda { |account| fake_remote_accounts_value }
+          config.remote_rentals_by_account = lambda { |account| fake_remote_rentals_by_account_value }
         end
         
         allow(controller).to receive(:render)
@@ -50,17 +58,17 @@ describe BookingsyncPortal::Admin::RentalsController do
       it "calls custom methods" do
         get :index
 
-        expect(BookingsyncPortal.not_connected_rentals.call(account)).to eq([fake_connected_rental])
-        expect(BookingsyncPortal.visible_rentals.call(account)).to eq([fake_visible_rental])
-        expect(BookingsyncPortal.remote_accounts.call(account)).to eq([fake_remote_account])
-        expect(BookingsyncPortal.remote_rentals_by_account.call(account)).to eq([fake_remote_rental])
+        expect(BookingsyncPortal.not_connected_rentals.call(account)).to eq(fake_connected_rentals_value)
+        expect(BookingsyncPortal.visible_rentals.call(account)).to eq(fake_visible_rental_value)
+        expect(BookingsyncPortal.remote_accounts.call(account)).to eq(fake_remote_accounts_value)
+        expect(BookingsyncPortal.remote_rentals_by_account.call(account)).to eq(fake_remote_rentals_by_account_value)
 
         expect(controller).to have_received(:render).at_least(1).times do |method, options|
           if method == :index
-            expect(options.dig(:locals, :not_connected_rentals)).to eq([fake_connected_rental])
-            expect(options.dig(:locals, :visible_rentals)).to eq([fake_visible_rental])
-            expect(options.dig(:locals, :remote_accounts)).to eq([fake_remote_account])
-            expect(options.dig(:locals, :remote_rentals_by_account)).to eq([fake_remote_rental])
+            expect(options.dig(:locals, :not_connected_rentals)).to eq(fake_connected_rentals_value)
+            expect(options.dig(:locals, :visible_rentals)).to eq(fake_visible_rental_value)
+            expect(options.dig(:locals, :remote_accounts)).to eq(fake_remote_accounts_value)
+            expect(options.dig(:locals, :remote_rentals_by_account)).to eq(fake_remote_rentals_by_account_value)
           end
         end
       end
