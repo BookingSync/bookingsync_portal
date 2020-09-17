5e929af4c62409310b0c64fe4ab02faaeecd30b8
Denis <denis.t@bookingsync.com>
2020-09-17 10:26:03 +0300

    Add rails 5.2
    fix sqlite3 config

--- spec/dummy/config/initializers/new_framework_defaults.rb
+++ spec/dummy/config/initializers/new_framework_defaults.rb
@@ -19,4 +19,6 @@ ActiveSupport.to_time_preserves_timezone = false
 # Require `belongs_to` associations by default. Previous versions had false.
 Rails.application.config.active_record.belongs_to_required_by_default = false
 
-Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+if Rails.application.config.active_record.respond_to(:sqlite3)
+  Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+end
